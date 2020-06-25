7d936faec6434038b4b079bfbfb6a21a919e4f49
Meliza Romo <melizza.romo.2098@gmail.com>
2020-06-24 23:09:04 -0700

    Version 3.1
    Deploy HEROKU

--- app/Regalo.php
+++ app/Regalo.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Regalo extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'regalos';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'regalo_id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['descripcion', 'precio'];
 }
